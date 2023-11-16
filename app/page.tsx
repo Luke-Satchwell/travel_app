3cef828ba8712ac286e33a5a5e185f5ea353c37d
Luke Satchwell <satch744@googlemail.com>
2023-11-16 23:42:47 +0000

    create page layout and hereo section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import CTA from "@/components/CTA";
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <CTA />
+    </>
+  );
 }
